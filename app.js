a2b2ab025e0c92af5364606f28d48c1892e30b41
Anhttt21 <anhttt21@fpt.com.vn>
2022-12-07 14:46:16 +0700

    config and using prettier

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -33,12 +33,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 route(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
