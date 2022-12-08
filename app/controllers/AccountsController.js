e466f93509e66c73660c50bed434b8439847f538
Anhttt21 <anhttt21@fpt.com.vn>
2022-12-08 15:19:44 +0700

    generate account controller

--- app/controllers/AccountsController.js
+++ app/controllers/AccountsController.js
@@ -3,12 +3,21 @@ const User = require('../models/User');
 class AccountsController {
   // [GET] /accounts
   accounts(req, res) {
-    User.getAll({}, function (err, users) {
-      if (!err) {
-        res.json(users);
-      }
-    });
-    // res.render('index', { title: 'Hello' });
+    // User.getAllUsers({}, function (err, users) {
+    //   if (!err) {
+    //     return res.json(users);
+    //   } else {
+    //     return res.json({ error: err });
+    //   }
+    // });
+
+    User.findAll({ limit: 5 })
+      .then((data) => {
+        return res.json(data);
+      })
+      .catch((error) => {
+        return res.json({ error: error });
+      });
   }
 }
 
